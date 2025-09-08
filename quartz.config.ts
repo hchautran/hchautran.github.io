38309cd5162f61d9a861c75ec7b25d64d7ab623a
Hoai-Chau Tran <tranhoaichau.00@gmail.com>
2025-09-08 16:25:26 -0500

    Quartz sync: Sep 8, 2025, 4:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Libre Baskerville",
-        body: "Noto Serif",
+        body: "PT Serif",
         code: "IBM Plex Mono",
       },
       colors: {
