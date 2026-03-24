e1e96c4851c8cf5aec5e6d44f244f3e13be32380
Hoai-Chau Tran <tranhoaichau.00@gmail.com>
2026-03-24 17:46:20 -0500

    Quartz sync: Mar 24, 2026, 5:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Hoai-Chau Tran",
+    pageTitle: "¯\\_(ツ)_/¯💻?",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
